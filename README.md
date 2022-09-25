d279200fd9117afa249225e8fc5775966e8683c7
nosam213 <101906569+nosam213@users.noreply.github.com>
2022-09-25 01:50:34 -0400

    Cleaned up functions and formatted for readability

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # krampus
 Krampus is a barebone replacement for Python's http.server, but with uploading, written in Go.
 
-curl -F "file=@filename.txt" http://0.0.0.0:9001/upload
+### Flags
+* --ssl true (default false)
+* --ssl port (default 9001)
+
+### SSL/TLS
+```
+$ ls
+cert.pem  key.pem
+$ doas ./krampus --ssl true --port 8443
+Server starting at port: 8443
+```
+
+## Uploading
+### Windows
+C:\\> curl -F "file=@\<file.txt\>" http[:]//127.0.0.1:9001/upload
+
+### Unix
+$ curl -F "file=@\<file.txt\>" http[:]//127.0.0.1:9001/upload
--- main.go
+++ main.go
@@ -8,34 +8,21 @@ import (
 	"os"
 )
 
-/*
-[X] Implement port selection through flags.
-[O] Pass the 'dirChoice' variable to FileUpload through handler.
-[X] Implement SSL trhough http.ListenAndServeTLS function. & if/else
-[O] Implement basic password authentication.
-[O] Implement a shit ton more logging for fatal errors, http module related
-*/
-
 // File Uploading
 func FileUpload(w http.ResponseWriter, r *http.Request) {
-	// FormFile to match arguments
 	file, fileHeader, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	defer file.Close()
 
-	// Creates uploads directory if not present
 	os.MkdirAll("./uploads", os.ModePerm)
 
-	// Create empty file 
 	dst, err := os.Create(fmt.Sprintf("./uploads/%s", fileHeader.Filename))
-
 	defer dst.Close()
 
-	// Copy file to destination
+	// Copy file[file] to destination[dst]
 	_, err = io.Copy(dst, file)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -45,10 +32,11 @@ func FileUpload(w http.ResponseWriter, r *http.Request) {
 
 // HTTP Routing
 func Routing(portChoice int16, sslChoice bool) {
-	// Formatting 'portChoice' to look like ex: ":8080" for http
 	var portChoiceFormatted string = fmt.Sprintf(":%d", portChoice)
+	FileDownload := http.FileServer(http.Dir("."))
 	
 	http.HandleFunc("/upload", FileUpload)
+	http.Handle("/", FileDownload)
 	
 	if sslChoice == true {
 		http.ListenAndServeTLS(portChoiceFormatted, "cert.pem", "key.pem", nil)
@@ -58,18 +46,13 @@ func Routing(portChoice int16, sslChoice bool) {
 }
 
 func main() {
-	// Flag Parsing
 	var portChoice int16 = 9001
 	pflag.Int16Var(&portChoice, "port", portChoice, "port selection (default 9001)")
-	/*
-	var dirChoice string = "./uploads"
-	pflag.StringVar(&dirChoice, "dir", dirChoice, "directory selection (default ./uploads")
-	*/
 	var sslChoice bool = false
 	pflag.BoolVar(&sslChoice, "ssl", sslChoice, "enables SSL, requires (exactly named) the 'cert.pem' and 'key.pem'")
 	
 	pflag.Parse()
 	
-	fmt.Printf("Server starting at port: %d", portChoice)
+	fmt.Printf("krampus(v1.1) starting at port: %d (SSL: %t)", portChoice, sslChoice)
 	Routing(portChoice, sslChoice)
 }
