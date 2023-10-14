0684215c9eeba48c9fc3875626f3a330c33e4192
nosam213 <101906569+nosam213@users.noreply.github.com>
2023-10-14 19:58:29 -0400

    fixed some formatting

--- main.go
+++ main.go
@@ -9,8 +9,9 @@ import (
 )
 
 const krampusVersion string = "1.2"
+
 var fileUploadPath string = "./uploads" // Default upload path
-var fileDownloadPath string = "./" // Default download path
+var fileDownloadPath string = "./"      // Default download path
 //var sslCertPath string = "./cert.pem"
 //var sslKeyPath string = "./key.pem"
 
@@ -34,8 +35,9 @@ func FileUpload(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-  fmt.Printf("[POST] %s \n", fileHeader.Filename)
+	fmt.Printf("[POST] %s \n", fileHeader.Filename)
 }
+
 /*
 // File Downloads
 func FileDownload(w http.ResponseWriter, r *http.Request) {
@@ -47,12 +49,12 @@ func FileDownload(w http.ResponseWriter, r *http.Request) {
 func Routing(portChoice int16, sslChoice bool) {
 	var portChoiceFormatted string = fmt.Sprintf(":%d", portChoice)
 	FileDownload := http.FileServer(http.Dir(fileDownloadPath))
-	
-  http.HandleFunc("/upload", FileUpload) // http://0.0.0.0:<port>/upload
-  http.Handle("/", FileDownload) // http://0.0.0.0:<port>/
-	
+
+	http.HandleFunc("/upload", FileUpload) // http://0.0.0.0:<port>/upload
+	http.Handle("/", FileDownload)         // http://0.0.0.0:<port>/
+
 	if sslChoice == true {
-    // Replace with sslCertPath / sslKeyPath functionality.
+		// Replace with sslCertPath / sslKeyPath functionality.
 		http.ListenAndServeTLS(portChoiceFormatted, "./cert.pem", "./key.pem", nil)
 	} else {
 		http.ListenAndServe(portChoiceFormatted, nil)
@@ -64,11 +66,11 @@ func main() {
 	pflag.Int16Var(&portChoice, "port", portChoice, "port selection")
 	var sslChoice bool = false
 	pflag.BoolVar(&sslChoice, "ssl", sslChoice, "enables SSL, requires (exactly named) the 'cert.pem' and 'key.pem'")
-  pflag.StringVar(&fileUploadPath, "file-upload-path", fileUploadPath, "file upload path")
-  pflag.StringVar(&fileDownloadPath, "file-download-path", fileDownloadPath, "file serve path")
-	
+	pflag.StringVar(&fileUploadPath, "file-upload-path", fileUploadPath, "file upload path")
+	pflag.StringVar(&fileDownloadPath, "file-download-path", fileDownloadPath, "file serve path")
+
 	pflag.Parse()
-	
+
 	fmt.Printf("krampus(v%s) starting at port: %d (SSL: %t)\n", krampusVersion, portChoice, sslChoice)
 	Routing(portChoice, sslChoice)
 }
